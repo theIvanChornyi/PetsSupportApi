3823c2ea8f0806a997de667976395b2b860ab3c2
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2023-01-18 16:57:09 +0200

    add  user endpoints

--- controllers/noticeControllers/addNotice.js
+++ controllers/noticeControllers/addNotice.js
@@ -1,28 +1,34 @@
 const Joi = require('joi');
 const Notice = require('../../models/noticeModel');
-const User = require('../../models/userModel');
 const cloudinary = require('../../services/cloudinary/cloudinary');
 
 const createError = require('../../helpers/createError');
-const { userNameRegExp, commentRegExp } = require('../../helpers/regExpressions');
+const {
+  userNameRegExp,
+  commentRegExp,
+} = require('../../helpers/regExpressions');
 
-const schemaNotice= Joi.object({
-  title: Joi.string().required().pattern(userNameRegExp).min(2).max(48),
+const schemaNotice = Joi.object({
+  title: Joi.string().required().pattern(commentRegExp).min(2).max(48),
   name: Joi.string().pattern(userNameRegExp).min(2).max(16),
   birthday: Joi.date(),
   breed: Joi.string().pattern(userNameRegExp).min(2).max(24),
-  sex: Joi.string().valid("male", "female"),
+  sex: Joi.string().valid('male', 'female'),
   location: Joi.string().required(),
   price: Joi.string().required().min(1),
   avatarURL: Joi.string(),
-  category: Joi.string().required().valid("lostFound", "inGoodHands", "sell").default('sell'),
+  category: Joi.string()
+    .required()
+    .valid('lostFound', 'inGoodHands', 'sell')
+    .default('sell'),
   comments: Joi.string().required().pattern(commentRegExp).min(8).max(120),
-})
+});
 
-
-const addNotice = async (req, res) => {
-
+const addNotice = async (req, res, next) => {
   let avatarURL = '';
+  if (!req.body?.notice) {
+    throw createError(400, 'Not found request param notice');
+  }
   if (req?.file) {
     const { secure_url } = await cloudinary.uploader.upload(req.file.path, {
       folder: '/userNotices',
@@ -31,22 +37,19 @@ const addNotice = async (req, res) => {
     avatarURL = secure_url;
   }
   const { _id: owner } = req.user;
-  const data = JSON.parse(req.body.notice)
+  const data = JSON.parse(req.body.notice);
 
   const { error } = await schemaNotice.validateAsync(data);
-    if (error) {
-      throw createError(400, "Missing required name field");
-    }
+  if (error) {
+    throw createError(400, 'Missing required name field');
+  }
 
   const notice = await Notice.create({
     ...data,
     avatarURL,
-    owner
+    owner,
   });
-    // await User.findByIdAndUpdate(owner, { $push: { notice: data._id } });
-
   return res.status(201).json(notice);
 };
 
 module.exports = addNotice;
-
