f51bb1050fa681067ec95866156c3546e9a95296
Kate3587 <108617406+Kate3587@users.noreply.github.com>
2023-01-21 20:41:15 +0100

    reg expression for prise fixed

--- helpers/regExpressions.js
+++ helpers/regExpressions.js
@@ -3,5 +3,5 @@ const passwordRegExp = /^\S*$/;
 const userNameRegExp = /^[a-zA-Z]+$/;
 const phoneRegExp = /^\+380\d{3}\d{2}\d{2}\d{2}$/;
 const commentRegExp = /\w/mi;
-const priceRegExp = /^[1-9]/;
+const priceRegExp = /^[1-9]\d*$/;
 module.exports = { emailRegExp, passwordRegExp, userNameRegExp, phoneRegExp, commentRegExp, priceRegExp };
