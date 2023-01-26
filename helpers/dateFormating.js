aba6c51d2c730102b816ee98c0648c6194cd1dfd
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2023-01-26 07:56:14 +0200

    add rule

--- helpers/dateFormating.js
+++ helpers/dateFormating.js
@@ -1,11 +1,12 @@
 const createError = require('./createError');
 const isDataCorect = require('./isDataCorect');
 
-const dateFormating = (date = new Date()) => {
-  if (!isDataCorect(date)) {
+const dateFormating = date => {
+  if (date && !isDataCorect(date)) {
     throw createError(400, 'Invalid day or month');
   }
-  const localeDate = date.toLocaleString('de-DE', {
+  const birthday = date || new Date();
+  const localeDate = birthday.toLocaleString('de-DE', {
     year: 'numeric',
     month: 'numeric',
     day: 'numeric',
